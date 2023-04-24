48c679716ae81247735cd6f94d2d64b3f1750cfe
Lavamgith <131001850+Lavamgith@users.noreply.github.com>
2023-04-24 19:19:05 -0400

    Update PAssign08.java

--- PAssign08.java
+++ PAssign08.java
@@ -20,50 +20,74 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PAssign08 extends Application {
-	// private Label
-	protected Label lblPhoneNumber;
+    // private Label
+    protected Label lblPhoneNumber;
 
-	@Override
-	public void start(Stage primaryStage) {
-		// a keyPad using KeyPadPane with a Label
-		KeyPadPane keyPad = new KeyPadPane(true);
-		lblPhoneNumber = new Label();
+    @Override
+    public void start(Stage primaryStage) {
+        // a keyPad using KeyPadCustomPane with a Label
+       // KeyPadPane keyPad = new KeyPadPane(true);
+    	KeyPadCustomPane keyPad = new KeyPadCustomPane(true);
+        lblPhoneNumber = new Label();
+        
+        
+        // An HBox area for the numbers to display when clicked
+        HBox numberDisplay = new HBox(lblPhoneNumber);
+        numberDisplay.setAlignment(Pos.CENTER_LEFT);
+        numberDisplay.setPadding(new Insets(10));
 
-		// An HBox area for the numbers to display when clicked
-		HBox numberDisplay = new HBox(lblPhoneNumber);
-		numberDisplay.setAlignment(Pos.CENTER_LEFT);
-		numberDisplay.setPadding(new Insets(10));
+        // a VBox for the scene of keyPad and numberDisplay
+        VBox VPane = new VBox(numberDisplay, keyPad);
+        VPane.setPadding(new Insets(50));
+        keyPad.setHgap(10); // horizontal gap between buttons
+        keyPad.setVgap(10); // vertical gap between buttons
 
-		// a VBox for the scene of keyPad and numberDisplay
-		VBox VPane = new VBox(numberDisplay, keyPad);
-		VPane.setPadding(new Insets(10));
-		keyPad.setHgap(10); // horizontal gap between buttons
-		keyPad.setVgap(10); // vertical gap between buttons
+        // event handler for * button from KeyPadCustomPane
+        keyPad.btnAsterisk.setOnAction(e -> {
+            lblPhoneNumber.setText(lblPhoneNumber.getText() + "*");
+            System.out.println("Button * was clicked.");
+        });
 
-		// for each buttons listed in KeyPadPane class
-		for (Button btKey : keyPad.listButtons) {
-			btKey.setOnAction(e -> {
-				String btText = btKey.getText();
-				lblPhoneNumber.setText(lblPhoneNumber.getText() + btText);
-			});
-		}
+        // event handler for # button from KeyPadCustomPane
+        keyPad.btnPound.setOnAction(e -> {
+            lblPhoneNumber.setText(lblPhoneNumber.getText() + "#");
+            System.out.println("Button # was clicked.");
+        });
 
-		//  event handler for * button from KeyPadPane
-		keyPad.btnAsterisk.setOnAction(e -> lblPhoneNumber.setText(lblPhoneNumber.getText() + "*"));
+        // Add Dialing button
+        Button btnDial = new Button("Dial");
+        btnDial.setOnAction(e -> {
+            String phoneNumber = lblPhoneNumber.getText();
+            System.out.println("Dialing " + phoneNumber);
+            // Printing phone call
+        });
+        
+        // Add Clear button
+        Button btnClear = new Button("Clear");
+        btnClear.setOnAction(e -> {
+            lblPhoneNumber.setText("");
+            System.out.println("Phone number cleared.");
+            // Printing phone number cleared
+        });
+        // HBoc for dial and clear
+        HBox hboxDialClear = new HBox(btnDial, btnClear);
+        hboxDialClear.setAlignment(Pos.CENTER_RIGHT);
+        hboxDialClear.setSpacing(10);
 
-		// event handler for # button from KeyPadPane
-		keyPad.btnPound.setOnAction(e -> lblPhoneNumber.setText(lblPhoneNumber.getText() + "#"));
+        VPane.getChildren().add(hboxDialClear);
+        VPane.setAlignment(Pos.CENTER);
+        VPane.setSpacing(10);
 
-		// Setting the stage
-		Scene scene = new Scene(VPane);
-		primaryStage.setTitle("Telephone KeyPad");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+        // Setting the stage
+        Scene scene = new Scene(VPane);
+        primaryStage.setTitle("Telephone KeyPad");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
-	}
+    }
 
-	// Launch
-	public static void main(String[] args) {
-		launch(args);
-	}
+    // Launch
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
